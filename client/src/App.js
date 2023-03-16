7f2ffabe8fce86ccba7108e94eeb152d2322e927
Sody15 <pdsoderb@gmail.com>
2023-03-15 19:37:03 -0700

    changes

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import Landing from "./components/layout/Landing";
 import Navbar from "./components/layout/Navbar";
 import Register from "./components/auth/Register";
 import Login from "./components/auth/Login";
+import Alert from "./components/layout/Alert";
 
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
@@ -17,6 +18,7 @@ const App = () => {
           <Navbar />
           <Route exact path="/" component={Landing} />
           <section className="container">
+            <Alert />
             <Switch>
               <Route exact path="/register" component={Register} />
               <Route exact path="/login" component={Login} />
